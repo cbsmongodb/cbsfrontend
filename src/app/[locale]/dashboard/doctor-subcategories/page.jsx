d998709e0ec882a7351a284bf70ef9bb9b65c82c
lizzaaabb <lbogveradze12@gmail.com>
2026-09-01 11:22:54 +0400

    translate doctor-subcategories page

--- src/app/[locale]/dashboard/doctor-subcategories/page.jsx
+++ src/app/[locale]/dashboard/doctor-subcategories/page.jsx
@@ -1,13 +1,19 @@
+'use client'
+
+import { useTranslations } from "next-intl";
 import ResourceTable from "@/components/ResourceTable/ResourceTable";
+
 export default function Page() {
+  const t = useTranslations();
+
   return (
     <ResourceTable
-      title="ექიმის ქვეკატეგორიები"
+      title={t('pages.doctorSubcategories')}
       endpoint="/api/doctor-subcategories"
       fields={[
-        { name: "name", label: "სახელი" },
-        { name: "description", label: "აღწერა" },
-        { name: "isActive", label: "აქტიური" },
+        { name: "name", label: t('fields.name') },
+        { name: "description", label: t('fields.description') },
+        { name: "isActive", label: t('fields.isActive') },
       ]}
     />
   );
